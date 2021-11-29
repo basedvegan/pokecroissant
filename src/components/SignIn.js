3068ea78a9a23d636e77739f195b281ce4a35744
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2021-11-28 22:39:01 -0800

    firebase connected with google signin

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { authentication } from '../firebase-config'
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function SignIn() {
+    const signInWithGoogle = ()=> {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(authentication, provider)
+        .then((re)=>{
+            console.log(re)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
     return (
         <div>
             sign In MotherFucker!!!!
+            <button onClick = {signInWithGoogle}>Niongle</button>
             <Link to = "/info"><button>Please Sign in</button></Link>
             
         </div>
